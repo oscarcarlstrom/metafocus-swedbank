81609ae3749d1250288ade131a6554b2712d3227
Oscar Carlström <oscar.carlstrom@metafocus.no>
2017-12-18 12:19:37 +0100

    Changed back to webshim

--- swedbank-script.js
+++ swedbank-script.js
@@ -11,7 +11,7 @@ var tabDown;
 var scrollToElementAddedByAJAX = false;
 
 //Use polyfill to enable numeric keypad on Android
-webshims.polyfill("forms");
+webshim.polyfill("forms");
 
 $(document).ready(function() {
 	//Remove browser validation
